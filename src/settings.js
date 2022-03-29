a04eb251bb3d2e6114014a97856563c7b2018280
Asif Khan <asifrafatkhan@me.com>
2022-03-29 21:48:06 +0400

    con string back to env

--- src/settings.js
+++ src/settings.js
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
 export const userEmail = process.env.USER_EMAIL;
-export const connectionString = 'mysql://woordenfabriekdb2:Andateam22!@34.141.207.214/woordenfabriek';
+export const connectionString = process.env.CONNECTION_STRING;
 export const secret = 'Super secret for the lazy brown fox in any string';
