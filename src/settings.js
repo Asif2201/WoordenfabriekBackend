7744728e676e991fb6919740dccd80c3710286eb
Asif Khan <asifrafatkhan@me.com>
2022-04-05 14:53:55 +0200

    trying with DO database

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,15 @@
 import dotenv from 'dotenv';
 
+let x = '';
+
 dotenv.config();
+if (process.env.DATABASE_URL) {
+  x = process.env.DATABASE_URL;
+} else {
+  x = process.env.CONNECTION_STRING;
+}
+
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
 export const userEmail = process.env.USER_EMAIL;
-export const connectionString = process.env.CONNECTION_STRING;
+export const connectionString = x;
 export const secret = 'Super secret for the lazy brown fox in any string';
